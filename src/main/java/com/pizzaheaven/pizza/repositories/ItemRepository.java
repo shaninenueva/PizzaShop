83bbae9d34333c7bb840e4058e809ec352a6627c
Shanine <nuevashanine@gmail.com>
2022-05-27 04:40:52 +1200

    fixed mapping errors

--- src/main/java/com/pizzaheaven/pizza/repositories/ItemRepository.java
+++ src/main/java/com/pizzaheaven/pizza/repositories/ItemRepository.java
@@ -3,5 +3,8 @@ package com.pizzaheaven.pizza.repositories;
 import com.pizzaheaven.pizza.models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
 }
