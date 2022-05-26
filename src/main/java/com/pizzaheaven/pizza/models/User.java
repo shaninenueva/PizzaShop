85a632f9694e0c52399affd85685c5a27cb609ae
Shanine <nuevashanine@gmail.com>
2022-05-26 16:40:14 +1200

    refactored to match db

--- src/main/java/com/pizzaheaven/pizza/models/User.java
+++ src/main/java/com/pizzaheaven/pizza/models/User.java
@@ -1,18 +1,79 @@
 package com.pizzaheaven.pizza.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name="Users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Long userID;
     private String name;
     private String type;
     private String street;
     private String suburb;
     private String city;
-    private int postCode;
+    private int post_code;
 
+    public User() {
+    }
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPost_code() {
+        return post_code;
+    }
+
+    public void setPost_code(int post_code) {
+        this.post_code = post_code;
+    }
 }
